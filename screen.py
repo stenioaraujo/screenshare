b95639a9251a923bfac63a32d433abf04ada616a
Stenio Elson <stenio.araujo@ccc.ufcg.edu.br>
2017-03-17 22:37:02 -0300

    Adding support to customize inputs, and fixing issues for windows

--- screen.py
+++ screen.py
@@ -1,21 +1,41 @@
-import pyscreenshot
 import flask
-from StringIO import StringIO
+from io import BytesIO
+import logging
+import os
+import pyscreenshot
+import sys
+
+if os.name == "nt":
+    from ctypes import windll
+    user32 = windll.user32
+    user32.SetProcessDPIAware()
 
 app = flask.Flask(__name__)
 
 
 @app.route('/screen.png')
 def serve_pil_image():
-    img_buffer = StringIO()
-    pyscreenshot.grab().save(img_buffer, 'PNG', quality=50)
+    quality = flask.request.args.get("quality", 10)
+    extension = flask.request.args.get("extension", "jpeg")
+    img_buffer = BytesIO()
+    pyscreenshot.grab().save(img_buffer, extension.upper(),
+                             quality=int(quality))
     img_buffer.seek(0)
-    return flask.send_file(img_buffer, mimetype='image/png')
+    return flask.send_file(img_buffer,
+                           mimetype='image/%s' % extension)
 
 
 @app.route('/')
 def serve_img():
     return flask.render_template('screen.html')
 
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
+    argv = dict(enumerate(sys.argv))
+    port = int(argv.get(1, 5000))
+    logging_level = argv.get(2, "NOTSET")
+    debug = argv.get(3) != "False"
+
+    logging.getLogger("werkzeug").setLevel(logging_level)
+
+    app.run(host='0.0.0.0', port=port, debug=debug)
